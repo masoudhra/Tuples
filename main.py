e358672f1d2c62685e4345431ae0429a9e31b691
Masoud Ramezani <masoudhra@gmail.com>
2023-09-04 14:16:12 +1200

    practice with tuple

---
+++ main.py
@@ -0,0 +1,12 @@
+# tafavote tuple va list in ast ke tuple ha heyre habele taghir hastand
+# be jaye beraket az parantez esteade mikonim
+shopping_list = ("Milk", "Bread", "Butter")
+
+print(shopping_list)
+print(shopping_list[1])
+print(shopping_list[:2])
+print(len(shopping_list))
+#ddastoore zir anjam nemishavad va khata midahad chon tuple taghir nemikonad
+# shopping_list[1] = "water"
+
+
